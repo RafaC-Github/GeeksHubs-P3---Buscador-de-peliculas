1ff22a31627bc1686edc0ae7a4511c4d9464fd81
RafaC-Github <61159155+RafaC-Github@users.noreply.github.com>
2020-03-10 17:58:31 +0100

    add search , keyup and fetched the api

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-axios.get('https://api.themoviedb.org/3/search/movie?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES&query='+busqueda)
-.then(res=>{
-    const peliculas =res.data.results
+var mymovies = document.querySelector(".mymovies");
+var searchInput = document.querySelector(".searchInput");
+searchInput.addEventListener('keyup',(event)=>{
+    if(event.key === 'Enter'){
+        getMovies(event.target.value)
+        event.target.value = ''
+    }
 })
-
-const getMovies =()=>{
-    fetch('https://api.themoviedb.org/3/search/movie?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES&query=')
-    .then(res=>res.json())
-    .then(movies=>{
-        document.querySelector('.movies').innerHTML='';
-        for (const movie of movies) {
-            document.querySelector('.movies').innerHTML+=`
-        <div class="movies">
-        </div>
+const getMovies =(busqueda)=>{
+    fetch('https://api.themoviedb.org/3/search/movie?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES&query='+busqueda)
+    .then(res=>res.json())//parsearlo a json
+    .then(res=>{
+        mymovies.innerHTML='';
+        const peliculas =res.results;
+        for (const pelicula of peliculas) {
+            mymovies.innerHTML+=`
+            <div class="movie">
+            <h1>${pelicula.title}</h1>
+            <h3>${pelicula.overview}<h3>
+            
+            </div>
             `
         }
     })
 }
+    
